0655e9b620a2b363c564b1c75a6fd7d3dcdeb3be
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-07-26 17:33:22 +0300

    chore: configure link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Link className={styles.link} href={'/pages/neon'}>click me</Link>
+        <Link className={styles.link} href={'/pages/timeline'}>click me</Link>
     </main>
   );
 }
