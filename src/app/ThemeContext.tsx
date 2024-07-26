374c4f8ef1384b6758f372fae19a1cdab837943f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-07-26 17:33:05 +0300

    chore: add ThemeProvider component

---
+++ src/app/ThemeContext.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, {createContext, useContext, useState, ReactNode, useEffect} from 'react';
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+interface ThemeContextProps {
+    isDarkTheme: boolean;
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+    const [isDarkTheme, setIsDarkTheme] = useState<boolean>(() => {
+        // Проверяем, сохранена ли тема в localStorage
+
+        if (typeof window !== 'undefined' && window.localStorage) {
+            // Проверяем, доступен ли localStorage в браузере
+            const savedTheme = localStorage.getItem('theme');
+            return savedTheme === 'dark';
+        } else {
+            return false; //Возвращаем значение по умолчанию, если localStorage не доступен
+        }
+    });
+
+    useEffect(() => {
+        // console.clear()
+
+        if (typeof window !== 'undefined' && window.localStorage) {
+            const savedTheme = localStorage.getItem('theme');
+            if (savedTheme) {
+                document.body.classList.toggle('darkTheme', savedTheme === 'dark');
+            }
+        }
+    }, []); // Вызываем эффект только при монтировании компонента
+
+    const toggleTheme = () => {
+        setIsDarkTheme((prevTheme) => !prevTheme);
+        const newTheme = isDarkTheme ? 'light' : 'dark';
+        localStorage.setItem('theme', newTheme);
+        document.body.classList.toggle('darkTheme');
+    };
+
+    const contextValue: ThemeContextProps = {
+        isDarkTheme,
+        toggleTheme,
+    };
+
+    return (
+        <ThemeContext.Provider value={contextValue}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = (): ThemeContextProps => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+
+    return context;
+};
+
